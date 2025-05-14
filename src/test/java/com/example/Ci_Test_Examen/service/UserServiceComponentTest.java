d65644ad48c709bb9e09609debc7cfc3d077e879
HafsaMU24 <3tomu24.abdaha@folkuniversitetet.nu>
2025-05-14 13:05:21 +0200

    Added workflow file

--- src/test/java/com/example/Ci_Test_Examen/service/UserServiceComponentTest.java
+++ src/test/java/com/example/Ci_Test_Examen/service/UserServiceComponentTest.java
@@ -1,15 +1,23 @@
 package com.example.Ci_Test_Examen.service;
 
 import com.example.Ci_Test_Examen.model.User;
+import com.example.Ci_Test_Examen.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@ActiveProfiles("test")
 @SpringBootTest
+@Transactional
 public class UserServiceComponentTest {
 
+    @Autowired
+    private UserRepository userRepository;
 
     @Autowired
     private UserService userService;
